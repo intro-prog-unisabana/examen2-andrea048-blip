48bfd5711f9ab933d7ae4bc83d5a7d92b8f0768e
Andrea Santamaria <santamariaandrea048@gmail.com>
2026-04-14 11:29:45 -0500

    ..

--- lap_timer.py
+++ lap_timer.py
@@ -11,8 +11,14 @@ def init(max_laps):
     """
     Crea y retorna un diccionario para almacenar hasta max_laps vueltas.
     """
+    diccionario = {
+        'max': max_laps,
+        'times': [],
+        'total': 0.0
+    }
+    return diccionario 
     # TODO: Implementar
-    pass
+    
 
 
 def add_lap(timer, time):
@@ -20,14 +26,17 @@ def add_lap(timer, time):
     Agrega una nueva vuelta con el tiempo especificado.
     Retorna el diccionario modificado.
     """
+    timer["times"].append(time)
+    timer["total"] += time
+    return timer
     # TODO: Implementar
-    pass
 
 
 def count(timer):
     """
     Retorna el numero de vueltas agregadas.
     """
+    return len(timer["times"])
     # TODO: Implementar
     pass
 
@@ -36,6 +45,7 @@ def cumulative_time(timer):
     """
     Retorna el tiempo acumulado de todas las vueltas.
     """
+    return round(timer["total"], 2)
     # TODO: Implementar
     pass
 
@@ -45,6 +55,7 @@ def format_laps(timer):
     Retorna una representacion en cadena de los tiempos.
     Formato: [t1, t2, t3, ..., tn]
     """
+    return str(timer["times"])
     # TODO: Implementar
     pass
 
@@ -53,6 +64,7 @@ def fastest_lap(timer):
     """
     Retorna el tiempo mas rapido de cualquier vuelta.
     """
+    return min(timer["times"])
     # TODO: Implementar
     pass
 
@@ -61,8 +73,17 @@ def fastest_multi_lap(timer, k):
     """
     Retorna el tiempo acumulado mas rapido de cualquier k vueltas consecutivas.
     """
+    times = timer["times"]
+    best = float("inf")
+
+    for i in range(len(times) - k + 1):
+        suma = sum(times[i:i+k])
+        if suma < best:
+            best = suma
+    return best
+
     # TODO: Implementar
-    pass
+    
 
 
 def longest_decreasing_streak(timer):
@@ -70,8 +91,20 @@ def longest_decreasing_streak(timer):
     Retorna la longitud maxima de una secuencia de vueltas consecutivas
     donde los tiempos disminuyen estrictamente.
     """
+    times = timer["times"]
+    max_streak = 1
+    current = 1
+
+    for i in range(1, len(times)):
+        if times[i] < times[i-1]:
+            current += 1
+            max_streak = max(max_streak, current)
+        else:
+            current = 1
+    
+    return max_streak
     # TODO: Implementar
-    pass
+    
 
 
 def main():
--- lap_timer_client.py
+++ lap_timer_client.py
@@ -8,16 +8,20 @@ import lap_timer
 def main():
     # TODO: Pedir el nombre del archivo al usuario usando input()
     
+    name_archivo = input("Ingrese el nombre del archivo: ")
+    
     # TODO: Abrir el archivo y leer el numero de vueltas n
     
-    # TODO: Crear el cronometro usando lap_timer.init(n)
+    with open(name_archivo, "r") as file:
+        n = int(file.readline().strip())
+        cronometro = lap_timer.init(n)
+        for _ in range(n):
+            tiempo = float(file.readline().strip())
+            cronometro = lap_timer.add_lap(cronometro, tiempo)
+    racha_min = lap_timer.longest_decreasing_streak(cronometro)
+    print(racha_min)
     
-    # TODO: Leer los n tiempos de vuelta y agregarlos con lap_timer.add_lap()
-    
-    # TODO: Imprimir la racha decreciente mas larga
-    #       usando lap_timer.longest_decreasing_streak()
-    
-    pass
+
 
 
 if __name__ == "__main__":
